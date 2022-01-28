ecada939c2e1de9d6190e0927eea8ca5109e51c3
Suelen Escorcio <suelen919@gmail.com>
2022-01-28 18:19:04 -0300

    Arquivo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import validator from "./validator.js";
 
-document.querySelector(".btn").addEventListener("click", Check);
 
-function Check(){
-    let pegaAqui=document.querySelector(".credito").value;
-    document.getElementById("cartao").innerHTML = pegaAqui.isValid;
+document.querySelector(".submit-btn").addEventListener("click", Check);
 
-    let pega=document.querySelector(".cvv-input").value;
-    document.getElementById("nomeTitular").innerHTML = pega;
+function Check() {
+    const CCredito = document.getElementById('credito');
+    const CCValidation = document.getElementById('ccValidator');
+    let message = "";
 
+    if(validator(CCredito.value) )
+        message = "Válido";
+    else
+        message = "Não Válido";
     
+    CCValidation.textContent = message;
+    CCredito.value = null;
+
+};
+
+
+
+document.querySelector('.cardNumber').oninput = () =>{
+    document.querySelector('.card-number-box').innerText = document.querySelector('.cardNumber').value;
 }
 
+document.querySelector('.cardNome').oninput = () =>{
+    document.querySelector('.card-name').innerText = document.querySelector('.cardNome').value;
+}
+
+document.querySelector('.inputMes').oninput = () =>{
+    document.querySelector('.expMes').innerText = document.querySelector('.inputMes').value;
+}
+
+document.querySelector('.inputAno').oninput = () =>{
+    document.querySelector('.expAno').innerText = document.querySelector('.inputAno').value;
+}
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,25 +1,30 @@
-const validator = {
-   isValid(credito) {
-      let length = credito.length;
-      let count = 0;
-
-      for(let i = 0; i < length; i++) {
-            let digito = parseInt(credito[i]);
-
-            if ( (i+2) % 2 === 0)
-                if((digito *= 2) > 9)
-                    digito -= 9;
-            
-            count += digito;
+    const validator = {
+        isValid(credito) {
+        let length = credito.length;
+        let count = 0;
+        let isValid = false;
+    // Percorre todo o número do cartão de crédito. 
+         for(let i = 0; i < length; i++) {
+                let digito = parseInt(credito[i]);
+               // Se o índice + 1 for par, dobre o valor.
+                if ( (i+2) % 2 === 0)
+                  // Se estiver acima de 9, ajuste o valor.
+                    if((digito *= 2) > 9)
+                        digito -= 9;
+                
+                count += digito;
+            }
+          // Se terminar com o final 0 é verdadeiro
+           if (count % 10 === 0){
+               isValid = true;
+           }
+        
+        return isValid;
         }
-    
-        return (count % 10) === 0;
     }
-}
 /*maskify(isValid) {
     return isValid.replace(/.(?=.{4})/g, "#");
-};let cardArr = Array.from(cardNumber).reverse;
-*/
+};*/
 
  export default validator;
  
